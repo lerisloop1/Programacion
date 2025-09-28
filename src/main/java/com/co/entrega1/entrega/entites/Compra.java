52f0a88ebc146937d6248676798220ff0b87dbf9
MarianaUL0707 <marianauribe318359@correo.itm.edu.co>
2025-09-28 17:12:55 -0500

    Se Agrego la clase Compra

---
+++ src/main/java/com/co/entrega1/entrega/entites/Compra.java
@@ -0,0 +1,30 @@
+package com.co.entrega1.entrega.entites;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "compra")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Compra {
+//
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Relación con Persona
+    @ManyToOne
+    @JoinColumn(name = "persona_id", referencedColumnName = "id", nullable = false)
+    private Persona persona;
+
+    // Relación con Moto
+    @ManyToOne
+    @JoinColumn(name = "moto_matricula", referencedColumnName = "matricula",nullable = false)
+    private Moto moto;
+
+    @Column(name = "fecha_compra", nullable = false)
+    private LocalDateTime fechaCompra;
+}
