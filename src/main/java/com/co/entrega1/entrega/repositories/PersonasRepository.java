443b4aa2c346d42c7555ce86605e0b546ae0b0dc
mateo ruiz <mateoruiz1120978@correo.itm.edu.co>
2025-11-11 23:26:56 -0500

    separacion de front end y back y inicio de frontend

--- src/main/java/com/co/entrega1/entrega/repositories/PersonasRepository.java
+++
@@ -1,9 +0,0 @@
-package com.co.entrega1.entrega.repositories;
-
-import com.co.entrega1.entrega.entites.Persona;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonasRepository extends JpaRepository<Persona, String> {
-}
